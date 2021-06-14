f9ce69d522ad4a73c30d959b4e758496b1f9cadc
hkopanski <khalid.kopanski@gmail.com>
2021-06-14 01:22:17 -0400

    Initial script to pull data v2

--- Proj_1_script.R
+++ Proj_1_script.R
@@ -22,10 +22,31 @@ df_franchise <- get_db("franchises")
 df_people <- get_db('people', '8477474')
 df_people
 #df_team_roster <- get_db("teams?expand=team.roster")
-df_temp <- get_db('teams', '?stats=statsSingleSeason&season=19801981')
+df_temp <- get_db('draft')
 df_temp
 print(df_teams)
 print(df_franchise)
+
+#/franchise    #(Returns id, firstSeasonId and lastSeasonId and name of every team in the history of the NHL)
+#/franchise-team-totals #(Returns Total stats for every franchise (ex roadTies, roadWins, etc))
+#/site/api/franchise-season-records?cayenneExp=franchiseId=ID #(Drill-down into season records for a specific franchise)
+#/franchise-goalie-records?cayenneExp=franchiseId=ID #(Goalie records for the specified franchise)
+#/franchise-skater-records?cayenneExp=franchiseId=ID #(Skater records, same interaction as goalie endpoint)
+#/site/api/franchise-detail?cayenneExp=mostRecentTeamId=ID #(Admin history and retired numbers)
+
+as_tibble(fromJSON(content(GET('https://statsapi.web.nhl.com/api/v1/standings?season=20052006'), 'text'), flatten = TRUE))
+
+as_tibble(fromJSON(content(GET('https://records.nhl.com/site/api/franchise-team-totals'), 'text'), flatten = TRUE))
+as_tibble(fromJSON(content(GET('https://records.nhl.com/site/api/franchise-team-totals'), 'text'), flatten = TRUE))
+
+as_tibble(fromJSON(content(GET('https://statsapi.web.nhl.com/api/v1/teams/?expand=team.roster'), 'text'), flatten = TRUE))
+
+as_tibble(fromJSON(content(GET('https://records.nhl.com/site/api/franchise-season-records?cayenneExp=franchiseId=6'), 'text'), flatten = TRUE))
+
+get_db('standings?season=20102011')
+
+#?expand=team.stats modifier
+#?stats=statsSingleSeason&season=19801981
 
 
 base <- "https://statsapi.web.nhl.com/api/v1/"
