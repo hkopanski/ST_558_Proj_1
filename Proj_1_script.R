1c20f37a758015c69d1ba63e87793c26205d8c17
hkopanski <khalid.kopanski@gmail.com>
2021-06-14 22:50:28 -0400

    Markdown Files and some additional queries one more time

--- Proj_1_script.R
+++ Proj_1_script.R
@@ -10,7 +10,7 @@ library(DBI)
 get_db_stats <- function(endpoint = NULL, modifier = NULL){
   base <- "https://statsapi.web.nhl.com/api/v1"
   
-  URL <- paste0(base, "/" , endpoint, "/" , modifier)
+  URL <- paste0(base, "/" , endpoint, modifier)
   temp_con <- GET(URL)
   temp_text <- content(temp_con, "text")
   temp_JSON <- fromJSON(temp_text, flatten = TRUE)
@@ -30,48 +30,15 @@ get_db_records <- function(endpoint = NULL, modifier = NULL){
 df_franchise <- get_db_records('franchise')    #(Returns id, firstSeasonId and lastSeasonId and name of every team in the history of the NHL)
 df_fran_team_tot <- get_db_records('franchise-team-totals') #(Returns Total stats for every franchise (ex roadTies, roadWins, etc))
 get_db_records('franchise-season-records?cayenneExp=franchiseId=16') #(Drill-down into season records for a specific franchise)
-get_db_records('franchise-goalie-records?cayenneExp=franchiseId=6') #(Goalie records for the specified franchise)
+df_goalie_briuns <- get_db_records('franchise-goalie-records?cayenneExp=franchiseId=6') #(Goalie records for the specified franchise)
 get_db_records('franchise-skater-records?cayenneExp=franchiseId=6') #(Skater records, same interaction as goalie endpoint)
 df_franchise_det <- get_db_records('franchise-detail') #?cayenneExp=mostRecentTeamId=6') #(Admin history and retired numbers)
 
+df_teams <- get_db_stats('teams')
+
 df_franchise2 <- get_db_stats('franchises')
 
-
-as_tibble(fromJSON(content(GET('https://statsapi.web.nhl.com/api/v1/standings?season=20052006'), 'text'), flatten = TRUE))
-
-as_tibble(fromJSON(content(GET('https://records.nhl.com/site/api/franchise-team-totals'), 'text'), flatten = TRUE))
-as_tibble(fromJSON(content(GET('https://records.nhl.com/site/api/franchise-team-totals'), 'text'), flatten = TRUE))
-
-as_tibble(fromJSON(content(GET('https://statsapi.web.nhl.com/api/v1/teams/?expand=team.roster'), 'text'), flatten = TRUE))
-
-as_tibble(fromJSON(content(GET('https://records.nhl.com/site/api/franchise-season-records?cayenneExp=franchiseId=6'), 'text'), flatten = TRUE))
-
-get_db('standings?season=20102011')
-
-#?expand=team.stats modifier
-#?stats=statsSingleSeason&season=19801981
+df_teams %>% inner_join(df_franchise, by = abbreviation)
 
 
-base <- "https://statsapi.web.nhl.com/api/v1/"
-endpoint <- "teams"
-value_x <- ""
-
-URL <- paste0(base, "/" , endpoint, "/" ,value_x)
-
-temp_con <- GET(URL)
-temp_text <- content(temp_con, "text")
-temp_JSON <- fromJSON(temp_text, flatten = TRUE)
-temp_tbl <- tbl_df(temp_JSON)
-print(temp_tbl)
-
-pikachu_text <- content(pikachu, "text")
-
-pikachu_JSON <- fromJSON(pikachu_text, flatten = TRUE)
-
-print(length(pikachu_JSON))
-
-pikachu_names <- names(pikachu_JSON)
-
-move_count <- nrow(pikachu_JSON$moves)
-
-game_ver <- nrow(pikachu_JSON$game_indices)
+ggplot(data = df_goalie_briuns) + geom_bar(aes(x = data$wins))
